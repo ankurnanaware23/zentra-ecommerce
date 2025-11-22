5598d6b6be33410e23634bd5fc83905229c9c34d
Ankur Nanaware <abhinanaware23@gmail.com>
2025-11-22 19:55:56 +0530

    updated the code to get media files while adding the imgaes in categopry field and added code for slug to get prepopulated

--- backend-drf/category/admin.py
+++ backend-drf/category/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Category
 
+# this class is used to auto populate the slug field based on category_name field
+class CategoryAdmin(admin.ModelAdmin):
+    # this will auto populate the slug field
+    prepopulated_fields = {'slug': ('category_name',)}
+    
+    # this will display the following fields in admin panel
+    list_display = ('category_name', 'slug', 'description', 'category_image')
+
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
