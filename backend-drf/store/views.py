c68e5137a95b9e7b170711d64756e2beb6cfadf4
Ankur Nanaware <abhinanaware23@gmail.com>
2025-11-22 23:29:41 +0530

    created the store page and created its view

--- backend-drf/store/views.py
+++ backend-drf/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def store(request):
+    products = Product.objects.all().filter(is_available = True)
+        # we will get all the info of products in product 
+
+    product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count
+    }    
+    return render(request, 'store/store.html', context)
