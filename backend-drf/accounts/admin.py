91a7de1fcfb707ec05c8f3f13d67c8eedfc130a2
Ankur Nanaware <abhinanaware23@gmail.com>
2025-11-22 19:36:01 +0530

    admin modification to readonly password

--- backend-drf/accounts/admin.py
+++ backend-drf/accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
+
+
+# we have creted this class to make the passord field as read only field
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_admin', 'is_staff', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 
 # Register your models here.
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
+
+admin.site.site_header = "Zentra Admin Panel"
